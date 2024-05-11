3990a32feb5017384078a7fa77a168bbc0547704
JON <jdlt@duck.com>
2024-05-11 13:54:34 +0200

    avances del sábado 11/05/24 en clase

--- form-validation.js
+++ form-validation.js
@@ -1,1 +1,23 @@
-console.log('Hola!');
+
+const userName = document.getElementById('inputName');
+const userMail = document.getElementById('inputEmail');
+const userPass1 = document.getElementById('inputPassword1');
+const userPass2 = document.getElementById('inputPassword2');
+
+const submitbtn = document.getElementById('submitbtn');
+
+function onSubmit(event) {
+    event.preventDefault();
+
+    const nameValue = userName.value;
+    const emailValue = userMail.value;
+    const pass1Value = userPass1.value;
+    const pass2Value = userPass2.value;
+
+    localStorage.setItem('userName', nameValue);
+    localStorage.setItem('userMail', emailValue);
+    localStorage.setItem('userPass1', pass1Value);
+    localStorage.setItem('userPass2', pass2Value);
+}
+
+submitbtn.addEventListener('click', onSubmit);
